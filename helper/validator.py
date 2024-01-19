f21214c27bd0bced01540f7adae7442322e280c1
NxPKG <116948796+NxPKG@users.noreply.github.com>
2024-01-19 03:09:53 +0000

    init comkmit

--- helper/validator.py
+++ helper/validator.py
@@ -10,7 +10,7 @@ conf = ConfigHandler()
 HEADER = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:34.0) Gecko/20100101 Firefox/34.0',
           'Accept': '*/*',
           'Connection': 'keep-alive',
-          'Accept-Language': 'zh-CN,zh;q=0.8'}
+          'Accept-Language': 'en-US,zh;q=0.8'}
 
 IP_REGEX = re.compile(r"(.*:.*@)?\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{1,5}")
 
